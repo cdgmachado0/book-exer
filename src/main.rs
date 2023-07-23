c55ec73ee94bdbc0d5e9bb77dbbb692e6e4fdf6e
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-07-23 19:43:36 +0200

    Done with fib sequence

--- src/main.rs
+++ src/main.rs
@@ -31,54 +31,57 @@ fn _convert_to_celsius() {
 }
 
 
-fn get_fibonacci() {
-    let mut i = 0;
-    let mut prev1: u32;
-    let mut prev2 = 1;
-    let mut fib_num: u32 = 1;
-
-    println!("What fibonacci number are you interested in?");
-
-    let input: u32 = loop {
+fn get_fibonacci() { 
+    'outter: loop {
+        println!("What fibonacci number are you interested in?");
         let mut input = String::new();
+        let mut i = 0;
+        let mut prev1: u64;
+        let mut prev2 = 1;
+        let mut fib_num: u64 = 1;
 
         io::stdin().read_line(&mut input).unwrap();
 
-        match input.trim().parse() {
-            Ok(num) => break num,
+        match input.trim().parse::<u64>() {
+            Ok(input) => {
+                if input == 0 {
+                    println!("Please input a number different than 0");
+                    continue 'outter;
+                }
+
+                let n_num = loop {
+                    if i == 0 {
+                        prev1 = 0;
+                        prev2 = 1;
+                    } else {
+                        prev1 = prev2;
+                        prev2 = fib_num;
+                    }
+            
+                    fib_num = prev1 + prev2;
+            
+                    i += 1;
+            
+                    if input == 2 {
+                        break prev2;
+                    } else if input == 1 {
+                        break prev1;
+                    } else if i == input - 2 {
+                        break fib_num;
+                    }
+            
+                    continue;
+                };
+
+                print!("The Nth fibonacci number is: {n_num}\n");
+                continue 'outter;
+            },
             Err(_) => {
                 println!("Please input a number");
                 continue;
             },
         };
     };
-
-
-    let n_num = loop {
-        if i == 0 {
-            prev1 = 0;
-            prev2 = 1;
-        } else {
-            prev1 = prev2;
-            prev2 = fib_num;
-        }
-
-        fib_num = prev1 + prev2;
-
-        i += 1;
-
-        if input == 2 {
-            break prev2;
-        } else if input == 1 {
-            break prev1;
-        } else if i == input - 2 {
-            break fib_num;
-        }
-
-        continue;
-    };
-
-    print!("The Nth fibonacci number is: {n_num}");
 
 
 }
