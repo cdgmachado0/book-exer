527b53fd70fc650e8325d8678510771441a0dc27
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-07-23 19:25:54 +0200

    Done (before making it infinite)

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ use std::io;
 
 
 fn main() {
-    convert_to_celsius();
+    // convert_to_celsius();
+
+    get_fibonacci();
 
 }
 
 
 
 
-fn convert_to_celsius() {
+fn _convert_to_celsius() {
     const FARENHEIT: f32 = 32.0;
     
     loop {
@@ -26,4 +28,57 @@ fn convert_to_celsius() {
             Err(_) => continue,
         };
     }
+}
+
+
+fn get_fibonacci() {
+    let mut i = 0;
+    let mut prev1: u32;
+    let mut prev2 = 1;
+    let mut fib_num: u32 = 1;
+
+    println!("What fibonacci number are you interested in?");
+
+    let input: u32 = loop {
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim().parse() {
+            Ok(num) => break num,
+            Err(_) => {
+                println!("Please input a number");
+                continue;
+            },
+        };
+    };
+
+
+    let n_num = loop {
+        if i == 0 {
+            prev1 = 0;
+            prev2 = 1;
+        } else {
+            prev1 = prev2;
+            prev2 = fib_num;
+        }
+
+        fib_num = prev1 + prev2;
+
+        i += 1;
+
+        if input == 2 {
+            break prev2;
+        } else if input == 1 {
+            break prev1;
+        } else if i == input - 2 {
+            break fib_num;
+        }
+
+        continue;
+    };
+
+    print!("The Nth fibonacci number is: {n_num}");
+
+
 }
